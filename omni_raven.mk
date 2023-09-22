284820882b3b8925de822ba2fb9929d582c65cd1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-22 19:15:45 +0200

    raven: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: google Model: Pixel 6 Pro Script version: 3.0.0

---
+++ omni_raven.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from raven device
+$(call inherit-product, device/google/raven/device.mk)
+
+PRODUCT_DEVICE := raven
+PRODUCT_NAME := omni_raven
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 6 Pro
+PRODUCT_MANUFACTURER := google
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 13 TQ3A.230805.001 10316531 release-keys"
+
+BUILD_FINGERPRINT := google/raven/raven:13/TQ3A.230805.001/10316531:user/release-keys
